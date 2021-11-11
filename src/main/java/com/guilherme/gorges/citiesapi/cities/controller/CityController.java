741923852810f19d88d440265c539e5e27ed373e
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-11-11 18:14:34 -0300

    Implement City: Service & Controller

--- src/main/java/com/guilherme/gorges/citiesapi/cities/controller/CityController.java
+++ src/main/java/com/guilherme/gorges/citiesapi/cities/controller/CityController.java
@@ -1,4 +1,25 @@
 package com.guilherme.gorges.citiesapi.cities.controller;
 
+import com.guilherme.gorges.citiesapi.cities.entity.City;
+import com.guilherme.gorges.citiesapi.cities.service.CityService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/cities")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class CityController {
+
+    private CityService cityService;
+
+    @GetMapping
+    public Page<City> getCities(final Pageable pageable){
+        return cityService.getCities(pageable);
+    }
 }
--- src/main/java/com/guilherme/gorges/citiesapi/cities/service/CityService.java
+++ src/main/java/com/guilherme/gorges/citiesapi/cities/service/CityService.java
@@ -1,4 +1,29 @@
 package com.guilherme.gorges.citiesapi.cities.service;
 
+import com.guilherme.gorges.citiesapi.cities.entity.City;
+import com.guilherme.gorges.citiesapi.cities.repository.CityRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class CityService {
+
+    private final CityRepository cityRepository;
+
+/*    //1st
+    public List<City> getCities(){
+        return cityRepository.findAll();
+    }*/
+
+    //2nd
+    public Page<City> getCities(final Pageable pageable){
+        return cityRepository.findAll(pageable);
+    }
+
 }
