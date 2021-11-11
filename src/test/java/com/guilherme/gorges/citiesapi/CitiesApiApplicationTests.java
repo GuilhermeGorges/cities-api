0a9a91044e5423ed6986dad773fe3b7b2a6525cf
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-11-11 14:27:14 -0300

    First Commit

---
+++ src/test/java/com/guilherme/gorges/citiesapi/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.gorges.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
